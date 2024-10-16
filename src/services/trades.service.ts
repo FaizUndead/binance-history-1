37a2cc899091ca4ed448b73f3012460d239d2560
VladLavrushko <vladislav.lavrushko@carrier.com>
2024-10-16 10:53:15 +0200

    implement service

--- src/services/trades.service.ts
+++ src/services/trades.service.ts
@@ -9,7 +9,7 @@ const client = binance({
 const limit = 1000;
 
 export const getTradesForThePeriod = async (symbol: string, startTime: number, endTime: number): Promise<AggregatedTrade[]> => {
-    let trades = await client.aggTrades({symbol: 'BTCUSDT', limit});
+    let trades = await client.aggTrades({symbol: 'BTCUSDT', startTime, endTime, limit});
     if(trades.length < limit) {
         return trades;
     }
@@ -17,9 +17,17 @@ export const getTradesForThePeriod = async (symbol: string, startTime: number, e
     let fromId = trades[trades.length - 1].aggId + 1;
 
     while(true) {
-        const currTrades = await client.aggTrades({symbol: 'BTCUSDT', limit, fromId: fromId.toString() });
+        let currTrades = await client.aggTrades({symbol: 'BTCUSDT', limit, fromId: fromId.toString() });
+
         
         if(currTrades.length === 0) break;
+
+        if (currTrades[currTrades.length - 1].timestamp > endTime) {
+            currTrades = currTrades.filter(trade => trade.timestamp < endTime);
+            trades.push(...currTrades);
+            break;
+        }
+
 
         trades.push(...currTrades);
 
@@ -27,20 +35,23 @@ export const getTradesForThePeriod = async (symbol: string, startTime: number, e
 
     }
 
-    console.log(trades);
-    console.log(trades.length);
+    console.log(trades, 'trades');
+    console.log(trades.length, 'trades.length');
+    console.log(trades[trades.length - 1], 'trades.length');
 
     return trades;
     
 };
 
   
-export const analizeTradesHistory = async (symbol: string, startTime: string, endTime: string): Promise<void> => {
+export const analizeTradesHistory = async (symbol: string, startTime: string, endTime: string): Promise<string> => {
     const startTimestamp = new Date(startTime).getTime();
     const endTimestamp = new Date(endTime).getTime();
     const trades = await getTradesForThePeriod(symbol, startTimestamp, endTimestamp);
-    console.log(trades);
-    
+
+    const result = (parseFloat(trades[trades.length - 1].price) - parseFloat(trades[0].price)) > 0 ? 'increased' : 'decreased';
+    // console.log(trades);
+    return result;
 };
 
 /*
